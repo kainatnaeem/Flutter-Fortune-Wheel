54d865047efb4b705f899c01defed9c385a98b4d
kainatnaeem <kainatnaeem59@gmail.com>
2023-01-21 01:39:58 +0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutterwheelbyktech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterwheelbyktech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutterwheelbyktech/fortunewheel.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Fortune Wheel ',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: const SpinWheel());
+  }
+}
